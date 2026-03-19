e5801bec71dcb7f3b165ea3b6e6e419b86a8fbec
Afra Lin <seatowntree@gmail.com>
2026-03-19 21:50:47 +0800

    NoteWebSite

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://AfraButera.github.io/AfraButera',
+  url: 'https://AfraButera.github.io',
   baseUrl: '/my-note/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
