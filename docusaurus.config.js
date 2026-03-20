b0a279932c846e68bf98de28612aa86845829b7f
Afra Lin <seatowntree@gmail.com>
2026-03-20 15:25:19 +0800

    NoteWebSite

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         items: [
           {
             type: 'doc',
-            docId: 'HELLO',
+            docId: 'HELLO/hello',
             position: 'left',
             label: '哈囉',
           },
