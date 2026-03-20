708137e85b3bb83a47e12acc31c2976fffb3cd86
Afra Lin <seatowntree@gmail.com>
2026-03-20 15:03:10 +0800

    NoteWebSite

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/hello world/hello">
             快速進入筆記區
           </Link>
         </div>
