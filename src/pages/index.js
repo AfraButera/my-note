b598a4333a3db1471b6cde8452148495fb85c316
Afra Lin <seatowntree@gmail.com>
2026-03-23 16:56:21 +0800

    NoteWebSite

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/src/hello/hello">
+            to="/src/hello/hello.md">
             LET'S SAY HI !
           </Link>
         </div>
